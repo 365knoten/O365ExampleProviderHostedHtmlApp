7f85a71b91108616ae53e401ec68ac6018f46e39
Sven Sieverding <sven@usetheco.de>
2019-01-15 09:34:15 +0100

    initial commit

---
+++ ProviderHostedHtmlApp/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="53e6f2ea-75b9-48bb-a1df-23469139b012" featureId="53e6f2ea-75b9-48bb-a1df-23469139b012" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ProviderHostedHtmlApp Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="bb7c1949-d509-42af-a45c-b193fb151ac6" />
+  </projectItems>
+</feature>
---
+++ ProviderHostedHtmlAppWeb/Scripts/Layout.js
@@ -0,0 +1,62 @@
+﻿var hostweburl;
+
+//load the SharePoint resources
+$(document).ready(function () {
+    //Get the URI decoded URL.
+    hostweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPHostUrl")
+        );
+
+    // The SharePoint js files URL are in the form:
+    // app_web_url/_layouts/15/resource
+    var scriptbase = hostweburl + "/_layouts/15/";
+
+    // Load the js file and continue to the 
+    //   success handler
+    $.getScript(scriptbase + "SP.UI.Controls.js", renderChrome)
+});
+
+//Function to prepare the options and render the control
+function renderChrome() {
+    // The Help, Account and Contact pages receive the 
+    //   same query string parameters as the main page
+    var options = {
+        "appIconUrl": hostweburl + "/_layouts/15/images/siteIcon.png",
+        "appTitle": "Provider Hosted App",
+        "appHelpPageUrl": "Help.html?"
+            + document.URL.split("?")[1],
+        "settingsLinks": [
+            {
+                "linkUrl": "Default.html?"
+                    + document.URL.split("?")[1],
+                "displayName": "Bacon"
+            },
+            {
+                "linkUrl": "ListData.html?"
+                    + document.URL.split("?")[1],
+                "displayName": "ListData"
+            }
+        ]
+    };
+
+    var nav = new SP.UI.Controls.Navigation(
+        "chrome",
+        options
+    );
+    nav.setVisible(true);
+}
+
+// Function to retrieve a query string value.
+// For production purposes you may want to use
+//  a library to handle the query string.
+function getQueryStringParameter(paramToRetrieve) {
+    var params =
+        document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
